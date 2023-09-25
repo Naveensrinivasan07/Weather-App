1e77caa04cf3775aab68ce35d8b6e0135af0630c
Naveensrinivasan07 <naveenkalz988@gmail.com>
2023-09-25 08:37:37 +0530

    final output

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react';
+import './App.css'
+import WeatherComponent from './Component/WeatherComponent/WeatherComponent'
+
+function App(){
+  return (
+    <React.Fragment>
+    <WeatherComponent></WeatherComponent>
+    </React.Fragment>
+  )
+  }
+
+export default App
---
+++ src/Component/WeatherComponent/WeatherComponent.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './WeatherComponent.css';
+
+const WeatherComponent = () => {
+  const [data, setData] = useState({});
+  const [cityName, setCityName] = useState('');
+
+  const weatherFunction = (event) => {
+    setCityName(event.target.value);
+  };
+
+  const fetchWeatherData = async () => {
+      const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=0b3b9d37958143d8a99134013231309&q=${cityName}`);
+      console.log(response.data);
+      setData(response.data);
+    
+  };
+
+  return (
+    <React.Fragment>
+         <div>
+        <center>
+        <div>
+        <h1> Entering your City Name to get weather condition:</h1>
+
+        <label htmlFor="cityName">Enter Your City Name: </label>
+        <input
+          type="text"
+          name="cityName"
+          id="cityName"
+          value={cityName}
+          placeholder="City Name"
+          onChange={weatherFunction}
+        />
+        <button onClick={fetchWeatherData}>Click</button>
+        {data.current && (
+          <div>
+            <h3>Location : {data.location.name}</h3>
+            <h3>Country : {data.location.country}</h3>
+            <h3>Temperature: {data.current.temp_c}&deg;C</h3>
+            <h3>Condition: {data.current.condition.text}</h3>
+            <h1><img src={data.current.condition.icon} alt="" /></h1>
+            <h3>Last Updated : {data.current.last_updated}</h3>
+            <h3>Latitude : {data.location.lat}</h3>
+            <h3>Longitude : {data.location.lon}</h3>
+            
+          </div>
+        )}
+      </div>
+      </center>
+    </div>
+    </React.Fragment>
+  );
+};
+
+export default WeatherComponent;
